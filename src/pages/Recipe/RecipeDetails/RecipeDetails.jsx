5eb1fe2c563a43f0f787ca92e03cc47a0fdb260f
hbashar434 <h.bashar434@gmail.com>
2023-05-03 00:29:00 +0600

    update ingredients

--- src/pages/Recipe/RecipeDetails/RecipeDetails.jsx
+++ src/pages/Recipe/RecipeDetails/RecipeDetails.jsx
@@ -10,6 +10,7 @@ const RecipeDetails = ({ recipes }) => {
     setFavorite(false);
     toast.success("The Recipe is your Favorite");
   };
+
   const CustomStar = (
     <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
   );
@@ -35,9 +36,19 @@ const RecipeDetails = ({ recipes }) => {
         <div className="grid items-center">
           <div className="card-body">
             <h2 className="card-title text-3xl">Recipe name : {recipe_name}</h2>
-            <p className=" text-xl">
-              <span className="text-2xl">Ingredients :</span> {ingredients}
-            </p>
+            <div className="flex justify-between mt-4">
+              <p className="text-3xl mr-8">Ingredients: </p>
+              <div className="grid md:grid-cols-3">
+                {ingredients?.map((ing, idx) => (
+                  <button
+                    className="btn btn-outline btn-info mr-2 mb-2"
+                    key={idx}
+                  >
+                    {ing}
+                  </button>
+                ))}
+              </div>
+            </div>
             <div className=" text-lg flex">
               <Rating
                 style={{ maxWidth: 150 }}
