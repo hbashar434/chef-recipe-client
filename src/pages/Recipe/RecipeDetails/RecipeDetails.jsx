17a119ba5315d7742e362e84b09f799dad7a2fc1
hbashar434 <h.bashar434@gmail.com>
2023-05-02 23:46:40 +0600

    upto react rating

--- src/pages/Recipe/RecipeDetails/RecipeDetails.jsx
+++ src/pages/Recipe/RecipeDetails/RecipeDetails.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const RecipeDetails = ({ recipes }) => {
   const { recipe_name, image, ingredients, cooking_method, rating } = recipes;
@@ -7,6 +9,17 @@ const RecipeDetails = ({ recipes }) => {
   const handleFavorite = () => {
     setFavorite(false);
     toast.success("The Recipe is your Favorite");
+  };
+  const CustomStar = (
+    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+  );
+  const myStyles = {
+    itemShapes: CustomStar,
+    itemStrokeWidth: 2,
+    activeFillColor: "LightSeaGreen",
+    activeStrokeColor: "#99F6E4",
+    inactiveFillColor: "#99F6E4",
+    inactiveStrokeColor: "LightSeaGreen",
   };
 
   return (
@@ -22,14 +35,24 @@ const RecipeDetails = ({ recipes }) => {
         <div className="grid items-center">
           <div className="card-body">
             <h2 className="card-title text-3xl">Recipe name : {recipe_name}</h2>
-            <p className=" text-xl">Ingredients : {ingredients}</p>
-            <p className=" text-lg">Ratings : {rating}</p>
+            <p className=" text-xl">
+              <span className="text-2xl">Ingredients :</span> {ingredients}
+            </p>
+            <div className=" text-lg flex">
+              <Rating
+                style={{ maxWidth: 150 }}
+                value={rating}
+                itemStyles={myStyles}
+                readOnly
+              />
+              <span className=" ml-2 text-2xl">{rating}</span>
+            </div>
           </div>
         </div>
       </div>
       <div className="text-lg font-semibold">
         {" "}
-        <span className="text-2xl">How to Cook :</span> {cooking_method}
+        <span className="text-2xl mr-2">How to Cook :</span> {cooking_method}
       </div>
       <div className="card-actions justify-end mt-4">
         <button
